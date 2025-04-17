d6608e9f57e4c8768f73764ef23ba78e48b65cc0
Hiromu Hota <hiromu.hota@gmail.com>
2025-04-17 20:06:22 +0000

    Revert "Try GC'ing the thing"
    This reverts commit 16cca7bae4d49c06d0467a752c57df84c31a49fa.

--- dask/base.py
+++ dask/base.py
@@ -1248,7 +1248,6 @@ def _normalize_pickle(o: object) -> tuple:
             try:
                 out = mod.dumps(o, protocol=5, buffer_callback=buffers.append)
                 tmp = mod.loads(out, buffers=buffers)
-                del tmp
                 pik2 = hash_buffer_hex(out)
             except Exception:
                 break
